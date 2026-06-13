0bf0407d614b1c0a7713ec4c4087cc1b2d2c307c
mario attrovio <mattrovio@measurestream-dev.polito.it>
2026-06-13 15:03:46 +0200

    fix init db

--- init-db.sql
+++ init-db.sql
@@ -5,25 +5,25 @@ WHERE NOT EXISTS (
         SELECT
         FROM pg_database
         WHERE datname = 'SETTINGS'
-    ) \ gexec
+    ) \gexec
 SELECT 'CREATE DATABASE "CALIBRATOR"'
 WHERE NOT EXISTS (
         SELECT
         FROM pg_database
         WHERE datname = 'CALIBRATOR'
-    ) \ gexec
+    ) \gexec
 SELECT 'CREATE DATABASE "SENSORS"'
 WHERE NOT EXISTS (
         SELECT
         FROM pg_database
         WHERE datname = 'SENSORS'
-    ) \ gexec -- Utente principale
+    ) \gexec -- Utente principale
     -- I permessi vengono assegnati se il database esiste
 GRANT ALL PRIVILEGES ON DATABASE "SENSORS" TO measurestream_admin;
 GRANT ALL PRIVILEGES ON DATABASE "SETTINGS" TO measurestream_admin;
 GRANT ALL PRIVILEGES ON DATABASE "CALIBRATOR" TO measurestream_admin;
 -- Connetti al database SENSORS e configura permessi per schema public
-\ c SENSORS;
+\c SENSORS;
 -- Assicurati che l'utente abbia i permessi necessari sullo schema public
 GRANT ALL PRIVILEGES ON SCHEMA public TO measurestream_admin;
 GRANT ALL ON SCHEMA public TO measurestream_admin;
